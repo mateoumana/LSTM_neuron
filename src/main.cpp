090727d70dc9f345325bb3cb19b21339acbe29a0
Mateo Umana <mateo.umana@mail.escuelaing.edu.co>
2025-03-08 18:16:59 -0500

    arreglo en los indices en los vectores

--- src/main.cpp
+++ src/main.cpp
@@ -20,18 +20,17 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, 22, 21, U8X8_PIN_NONE);//CLK; 
 
 #define TIME_STEPS 12
 
-void read_file(String ,int ,int ,float *);// should start with /
+void read_file(String ,int ,int ,float *);
 float LSTM_neuron(float *, float *, float *, float *, float *, float *, float *, float *, float *);
 void dense_neuron(float *, float *, float *, const char *);
 float sigmoid(float *);
 
+//Esperando un vector de pesos de la siguiente forma: sesgo+pesos
 float dense_f_weights[1][TIME_STEPS + 1] = {0};//+ 1 por el sesgo
 float dense_i_weights[1][TIME_STEPS + 1] = {0};
 float dense_C_weights[1][TIME_STEPS + 1] = {0};
 float dense_o_weights[1][TIME_STEPS + 1] = {0};
-float sesgo = 0; 
-float x_t[1][TIME_STEPS + 1] = {0};//para incluir h_t_1
-
+//recorrer matrices con punteros: *(matriz + i * columnas + j) = matriz[i][j]
 
 void setup() {
   pinMode(2,OUTPUT);
@@ -49,28 +48,28 @@ void setup() {
   Serial.printf("Total space: %d\n",LittleFS.totalBytes());
   Serial.printf("Total space used: %d\n",LittleFS.usedBytes());
   Serial.println("========================\n");
-
-  read_file("/weights_dense_f.txt",1,TIME_STEPS,(float *)(dense_f_weights + 1));//el primer espacio es para h_t_1
-  read_file("/weights_dense_i.txt",1,TIME_STEPS,(float *)(dense_i_weights + 1));
-  read_file("/weights_dense_C.txt",1,TIME_STEPS,(float *)(dense_C_weights + 1));
-  read_file("/weights_dense_o.txt",1,TIME_STEPS,(float *)(dense_o_weights + 1));
-  sesgo = dense_f_weights[0][0]; //el primer dato es el sesgo
+  
+  read_file("/weights_dense_f.txt",1,TIME_STEPS+1,(float *)(dense_f_weights));// should start with /
+  read_file("/weights_dense_i.txt",1,TIME_STEPS+1,(float *)(dense_i_weights));
+  read_file("/weights_dense_C.txt",1,TIME_STEPS+1,(float *)(dense_C_weights));
+  read_file("/weights_dense_o.txt",1,TIME_STEPS+1,(float *)(dense_o_weights));
   delay(1000); 
 }
 
 
 void loop() {
-  if (Serial.available()){ 
-    float C_t_1 = 0; float C_t = 0;   
-    float h_t = 0; float h_t_1 = 0; 
-    LSTM_neuron((float *)(dense_f_weights + 1),
-                (float *)(dense_C_weights + 1),
-                (float *)(dense_i_weights + 1),
-                (float *)(dense_o_weights + 1),
-                (float *)(x_t));
+  if (Serial.available()){ //presionar enter para ejecutar la neurona
+    float C_t = 0; float C_t_1 = 0;//segun la red estos valores se definen en otro lugar, por ahora es solo para una ejecución
+    float h_t = 0; float h_t_1 = 0;
+    float x_t[1][TIME_STEPS] = {0};//entradas
+    LSTM_neuron((float *)(dense_f_weights),//se pasa todo el vector y en la funcion se separa el sesgo
+                (float *)(dense_C_weights),
+                (float *)(dense_i_weights),
+                (float *)(dense_o_weights),
+                (float *)(x_t), &h_t, &h_t_1, &C_t, &C_t_1);
     Serial.printf("h_t: %.8f\n",h_t);
     u8g2.drawStr(15,55,"h_t:");
-    u8g2.setCursor(95, 55); u8g2.print(String(h_t));
+    u8g2.setCursor(55, 55); u8g2.print(String(h_t));
     u8g2.sendBuffer();
   }
 }
@@ -104,26 +103,26 @@ float LSTM_neuron(float *f_weights, float *C_weights, float *i_weights, float *o
   float f_t = 0;   float i_t = 0;
   float o_t = 0;   float _C_t = 0;//~C_t
   //Ejecucion del modelo
-  *(x + 1) = *h_t_1;//se añade h_t_1 a la entrada general, al inicio
-  dense_neuron((float *)(dense_f_weights + 1), (float *)x_t, &f_t, "sigmoid"); //calculo de f_t
-  dense_neuron((float *)(dense_C_weights + 1), (float *)x_t, &_C_t, "sigmoid"); //calculo de i_t
-  dense_neuron((float *)(dense_i_weights + 1), (float *)x_t, &i_t, "tanh");    //calculo de _C_t
-  dense_neuron((float *)(dense_o_weights + 1), (float *)x_t, &o_t, "sigmoid"); //calculo de o_t
+  *(x + 0) = *h_t_1;//se añade h_t_1 a la entrada general, al inicio
+  dense_neuron((float *)(dense_f_weights), (float *)x, &f_t, "sigmoid"); //calculo de f_t
+  dense_neuron((float *)(dense_C_weights), (float *)x, &_C_t, "sigmoid"); //calculo de i_t
+  dense_neuron((float *)(dense_i_weights), (float *)x, &i_t, "tanh");    //calculo de _C_t
+  dense_neuron((float *)(dense_o_weights), (float *)x, &o_t, "sigmoid"); //calculo de o_t
   *C_t = f_t*(*C_t_1) + i_t*_C_t;
   *h_t = tanh(*C_t)*o_t;
   *h_t_1 = *h_t;//guardar el valor anterior
   *C_t_1 = *C_t;
 }
 
-void dense_neuron(float *wieghts, float *ints, float *result, const char *func_act){
+void dense_neuron(float *weights, float *intput, float *result, const char *func_act){
   for(int i = 0; i < TIME_STEPS; i++){
-    *result += *(wieghts + i) * *(ints + i);
+    *result += *(weights + i + 1) * *(intput + i); //weights + 1 para evadir el sesgo
   }
-  *result += *(wieghts + TIME_STEPS + 1);//se suma el sesgo
+  *result += *(weights + 0);//se suma el sesgo (primera posición)
   if(strcmp(func_act, "sigmoid") == 0){
-    *result += sigmoid(result);//se aplica la funcion de activacion
+    *result = sigmoid(result);//se aplica la funcion de activacion
   }else if(strcmp(func_act, "tanh") == 0){
-    *result += tanh(*result);//se aplica la funcion de activacion
+    *result = tanh(*result);  //se aplica la funcion de activacion
   }
 }
 
